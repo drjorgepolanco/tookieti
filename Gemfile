49784ec05230d6f5bf7d8058a63aa50e5c5036b0
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-03-17 12:46:10 -0400

    Create first tests and static pages, add foundation

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'hirb'
 gem 'haml-rails'
+gem 'foundation-rails'
 
 
 # Use ActiveModel has_secure_password
---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe "Pages", type: :request do
+
+  let(:base_title) { "Tookieti" }
+
+  describe "Home page" do
+    before { visit "/pages/home" }
+
+    it "should have the content 'Tookieti'" do
+      expect(page).to have_content("#{base_title}")
+    end
+
+    it "should have title 'Tookieti'" do
+      expect(page).to have_title("#{base_title}")
+    end
+  end
+
+  describe "About page" do
+    before { visit "pages/about" }
+    
+    it "should have the content 'About'" do
+      expect(page).to have_content('About')
+    end
+
+    it "should have title 'About | Tookieti'" do
+      expect(page).to have_title("About | #{base_title}")
+    end
+  end
+end
