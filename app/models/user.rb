fd02cff1578b6c518bd9c3a0bec212a23b8ceb9f
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-03-17 23:02:26 -0400

    Add valid email regex and test email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
+  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :first_name, presence: true, length: { maximum: 40 }
   validates :last_name,  presence: true, length: { maximum: 40 }
-  validates :email,      presence: true, length: { maximum: 80 }
+  validates :email,      presence: true, length: { maximum: 80 },
+                         format: { with: VALID_EMAIL }
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,4 +43,26 @@ RSpec.describe User, type: :model do
     before { @user.email = "x" * 81      }
     it     { is_expected.to_not be_valid }
   end
+
+  describe "when email format is not valid" do
+    it "should not be valid" do
+      emails = %w[tri_cu_li_to.mail tricu.lito@mail. triculito@mail,com 
+                  tricu@lito_mail.com triculito@mail#com triculito@++mail.com]
+      emails.each do |invalid_email|
+        @user.email = invalid_email
+        expect(@user).to_not be_valid
+      end
+    end
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      emails = %w[triculito@mail.com Tricu_Lito@mail.COM triculi-to@mail.us
+                  dr.triculito@mail.org]
+      emails.each do |valid_email|
+        @user.email = valid_email
+        expect(@user).to be_valid
+      end
+    end
+  end
 end
