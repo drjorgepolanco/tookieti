6481fd1cc9ec4b2264b61dc39f92429d5f2dbf2c
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-03-19 10:56:33 -0400

    refactor flash msgs and add options to gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,9 +3,10 @@ module UsersHelper
     "#{user.first_name.capitalize} #{user.last_name.capitalize}"
   end
 
-  def gravatar(user)
+  def gravatar(user, options = { size: 80 })
     id  = Digest::MD5::hexdigest(user.email.downcase)
-    url = "https://secure.gravatar.com/avatar/#{id}"
+    size = options[:size]
+    url = "https://secure.gravatar.com/avatar/#{id}?s=#{size}"
     image_tag(url, alt: full_name(user), class: "gravatar")
   end
 end
