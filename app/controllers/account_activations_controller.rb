48ddf5e6965395bdf254d0b75e8523e3c6a25a37
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-03-28 16:47:02 -0400

    Finish account activations, create model methos activate() and send_activation_email() to refactor AccountActivations and UsersController

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -4,9 +4,8 @@ class AccountActivationsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && 
       !user.activated? && 
-       user.authenticated?(:activation,     params[:id])
-       user.update_attribute(:activated,    true)
-       user.update_attribute(:activated_at, Time.zone.now)
+       user.authenticated?(:activation, params[:id])
+       user.activate
        log_in(user)
        flash[:success] = "Your account is now activated!"
        redirect_to(user)
--- spec/requests/users_signups_spec.rb
+++ spec/requests/users_signups_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "UsersSignups", type: :request do
+
+  before { ActionMailer::Base.deliveries.clear }
+
   describe "signing up with invalid information" do
     it "should not work and should go back to the signup form" do
       get signup_path
@@ -18,11 +21,11 @@ RSpec.describe "UsersSignups", type: :request do
     end
   end
 
-  describe "signing up with valid information" do
+  describe "signing up with valid information and account activation" do
     it "should work and should redirect to user's show view" do
       get signup_path
       expect do
-        post_via_redirect users_path, user: { 
+        post users_path, user: { 
           first_name:            "Julito",
           last_name:             "Triculi",
           email:                 "triculito@gmail.com",
@@ -30,8 +33,20 @@ RSpec.describe "UsersSignups", type: :request do
           password_confirmation: "worldtriculi"
         }
       end.to change{ User.count }.from(0).to(1)
-      # expect(response).to render_template(:show)
-      expect(flash[:info]).to_not be(nil)
+      expect(ActionMailer::Base.deliveries.size).to eq(1)
+      user = assigns(:user)
+      expect(user.activated?).to be(false)
+      log_in_as(user)
+      expect(is_logged_in?).to be(false)
+      get edit_account_activation_path("anytokenbutthecorrectone")
+      expect(is_logged_in?).to be(false)
+      get edit_account_activation_path(user.activation_token, email: "yoohoo")
+      expect(is_logged_in?).to be(false)
+      get edit_account_activation_path(user.activation_token, email: user.email)
+      expect(user.reload.activated?).to be(true)
+      follow_redirect!
+      expect(response).to render_template(:show)
+      expect(is_logged_in?).to be(true)
     end
   end
 end
