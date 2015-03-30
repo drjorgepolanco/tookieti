e856d1e1b4f2bb1b6009e39b136766f1e64c7aed
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-03-29 21:48:57 -0400

    Modularize and update tests to use contexts, single expectations and better specs

--- spec/requests/password_resets_spec.rb
+++ spec/requests/password_resets_spec.rb
@@ -1,99 +1,139 @@
 require 'rails_helper'
 
 RSpec.describe "PasswordResets", type: :request do
+
   before do
     ActionMailer::Base.deliveries.clear
     @user = FactoryGirl.create(:user)
   end
 
-  describe "password resets" do
-    it "should not work with invalid info or expired token and should work with the opposite" do
+  describe "requesting password reset" do
+
+    before do
       get new_password_reset_path
       expect(response).to render_template(:new)
+    end
 
-      # invalid email
-      post password_resets_path, password_reset: { email: "" }
-      expect(flash).to_not be(nil)
-      expect(response).to render_template(:new)
+    context "when email is invalid" do
+      it "should display flash message and return to form" do
+        post password_resets_path, password_reset: { email: "" }
+        expect(flash).to_not be(nil)
+        expect(response).to render_template(:new)
+      end
+    end
 
-      # valid email
+    context "when email is valid" do
+      it "should send password reset link and redirect to root" do
+        post password_resets_path, password_reset: { email: @user.email }
+        expect(@user.reload.reset_digest).to eq(@user.reset_digest)
+        expect(ActionMailer::Base.deliveries.size).to eq(1)
+        expect(flash).to_not be(nil)
+        expect(response).to redirect_to(root_url)
+      end
+    end
+  end
+
+  describe "getting the password reset form" do
+
+    before do
+      get new_password_reset_path
       post password_resets_path, password_reset: { email: @user.email }
-      expect(@user.reload.reset_digest).to eq(@user.reset_digest)
-      expect(ActionMailer::Base.deliveries.size).to eq(1)
-      expect(flash).to_not be(nil)
-      expect(response).to redirect_to(root_url)
+      @user = assigns(:user)
+    end
 
-      # password reset form
-      user = assigns(:user)
+    context "when email is wrong" do
+      it "should redirect to root" do
+        get edit_password_reset_path(@user.reset_token, email: "")
+        expect(response).to redirect_to(root_url)
+      end
+    end
 
-      # wrong email
-      get edit_password_reset_path(user.reset_token, email: "")
-      expect(response).to redirect_to(root_url)
+    context "when user is inactive" do
+      it "should redirect to root" do
+        @user.toggle!(:activated)
+        get edit_password_reset_path(@user.reset_token, email: @user.email)
+        expect(response).to redirect_to(root_url)
+      end
+    end
 
-      # inactive user
-      user.toggle!(:activated)
-      get edit_password_reset_path(user.reset_token, email: user.email)
-      expect(response).to redirect_to(root_url)
-      user.toggle!(:activated)
+    context "when email is right and token is wrong" do
+      it "should redirect to root" do
+        get edit_password_reset_path('tukitoken', email: @user.email)
+        expect(response).to redirect_to(root_url)
+      end
+    end
 
-      # right email, wrong token
-      get edit_password_reset_path('tukitoken', email: user.email)
-      expect(response).to redirect_to(root_url)
+    context "when both email and token are correct" do
+      it "should render the edit form to create a new password" do
+        get edit_password_reset_path(@user.reset_token, email: @user.email)
+        expect(response).to render_template(:edit)
+        expect(response.body).to include('<input type="hidden" name="email" id="email" value="triculito@mail.com" />')
+      end
+    end
 
-      # right email, right token
-      get edit_password_reset_path(user.reset_token, email: user.email)
-      expect(response).to render_template(:edit)
-      expect(response.body).to include('<input type="hidden" name="email" id="email" value="triculito@mail.com" />')
+    context "when token is expired" do
+      it "should include the word expired" do
+        @user.update_attribute(:reset_sent_at, 3.hours.ago)
+        patch password_reset_path(@user.reset_token),
+              email: @user.email,
+              user: { 
+                password:           "password",
+                password_confirmation: "password"
+              }
+        expect(response).to be_redirect
+        follow_redirect!
+        expect(response.body).to include('expired')
+      end
+    end
+  end
 
-      # invalid password & confirmation
-      patch password_reset_path(user.reset_token),
-            email: user.email,
+  describe "creating the new password" do
+
+    before do
+      get new_password_reset_path
+      post password_resets_path, password_reset: { email: @user.email }
+      @user = assigns(:user)
+      get edit_password_reset_path(@user.reset_token, email: @user.email)
+    end
+
+    context "when password and confirmation doesn't match" do
+      it "should display error message" do
+        patch password_reset_path(@user.reset_token),
+            email: @user.email,
             user: {
               password:              "worldtriculi",
               password_confirmation: "triculito"
             }
-      expect(response.body).to include("<div id='error_explanation'>")
-      expect(flash).to_not be(nil)
+        expect(response.body).to include("<div id='error_explanation'>")
+        expect(flash).to_not be(nil)
+      end
+    end
 
-      # blank password
-      patch password_reset_path(user.reset_token),
-            email: user.email,
+    context "when password is blank" do
+      it "should display flash message and stay in the form" do
+        patch password_reset_path(@user.reset_token),
+            email: @user.email,
             user: { 
               password:              "",
               password_confirmation: "triculito" 
             }
-      expect(flash).to_not be(nil)
-      expect(response).to render_template(:edit)
-
-      # valid password & confirmation
-      patch password_reset_path(user.reset_token),
-            email: user.email,
-            user: {
-              password:              "triculito",
-              password_confirmation: "triculito"
-            }
-      expect(is_logged_in?).to be(true)
-      expect(flash).to_not be(nil)
-      expect(response).to redirect_to(user)
+        expect(flash).to_not be(nil)
+        expect(response).to render_template(:edit)
+      end
     end
-  end
 
-  context "when token is expired" do
-    it "should include the word expired" do
-      get new_password_reset_path
-      post password_resets_path, password_reset: { email: @user.email }
-
-      @user = assigns(:user)
-      @user.update_attribute(:reset_sent_at, 3.hours.ago)
-      patch password_reset_path(@user.reset_token),
-            email: @user.email,
-            user: { 
-              password:           "password",
-              password_confirmation: "password"
-            }
-      expect(response).to be_redirect
-      follow_redirect!
-      expect(response.body).to include('expired')
+    context "when password and confirmation match" do
+      it "should log the user in and redirect to user profile" do
+        patch password_reset_path(@user.reset_token),
+              email: @user.email,
+              user: {
+                password:              "triculito",
+                password_confirmation: "triculito"
+              }
+        expect(is_logged_in?).to be(true)
+        expect(flash).to_not be(nil)
+        expect(response).to redirect_to(@user)
+      end
     end
   end
 end
