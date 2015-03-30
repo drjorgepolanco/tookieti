5a98d67f54a7df4428f9f23d82c5d619258ebac7
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-03-29 22:33:39 -0400

    Write better specs for users logins

--- spec/requests/users_logins_spec.rb
+++ spec/requests/users_logins_spec.rb
@@ -3,52 +3,92 @@ require 'rails_helper'
 RSpec.describe "Users Logins", type: :request do
 
   before do
-    @user = FactoryGirl.create(:user, :first_name => "Julito",
+    @user = FactoryGirl.create(:user, 
+                               :first_name => "Julito",
                                :last_name => "Triculi", 
                                :email => "triculito@gmail.com",
                                :password_digest => User.digest("password"))
   end
 
-  describe "with invalid information" do
-    it "should not work and flash msg should not persist" do
+  context "when filling the form" do
+
+    before do
       get login_path
-      expect(response).to render_template(:new)
-      post login_path, session: { email: "", password: "" }
-      expect(response).to render_template(:new)
-      expect(flash[:alert]).to_not be_nil
-      get root_path
-      expect(flash[:alert]).to be_nil
     end
-  end
 
-  describe "with valid information followed by logout" do
-    it "should work and first redirect to user's show and then to root" do
-      get login_path
+    it "should render login form" do
       expect(response).to render_template(:new)
-      post login_path, session: { email: @user.email, password: "password" }
-      expect(is_logged_in?).to be(true)
-      expect(response).to redirect_to(@user)
-      follow_redirect!
-      expect(response).to render_template(:show)
-      expect(response.body).to include("Log out")
-      expect(response.body).to include("Profile")
-      delete logout_path
-      expect(is_logged_in?).to be(false)
-      expect(response).to redirect_to(root_path)
-      delete logout_path
-      follow_redirect!
-      expect(response.body).to include("Log In")
-      expect(response.body).to include("Sign Up")
     end
-  end
 
-  it "is possible with remembering" do
-    log_in_as(@user, remember_me: '1')
-    expect(cookies['remember_token']).to eq(assigns(:user).remember_token)
-  end
+    context "when fields are empty" do
+      it "should stay in the form and flash msg should not persist" do
+        post login_path, session: { 
+          email: "", 
+          password: "" 
+        }
+        expect(response).to render_template(:new)
+        expect(flash[:alert]).to_not be_nil
+        get root_path
+        expect(flash[:alert]).to be_nil
+      end
+    end
 
-  it "is possible without remembering" do
-    log_in_as(@user, remember_me: '0')
-    expect(cookies['remember_token']).to be(nil)
+    context "when info is valid" do
+      
+      before {
+        post login_path, session: { 
+          email: @user.email, 
+          password: "password" 
+        }
+      }
+
+      it "user should be taken to his profile" do
+        expect(is_logged_in?).to be(true)
+        expect(response).to redirect_to(@user)
+        follow_redirect!
+        expect(response).to render_template(:show)
+        expect(response.body).to include("Log out")
+        expect(response.body).to include("Profile")
+      end
+
+      context "followed by logout" do
+
+        before do
+          expect(response).to redirect_to(@user)
+          follow_redirect!
+          delete logout_path
+        end
+
+        it "should log the user out and redirect to root" do
+          expect(is_logged_in?).to be(false)
+          expect(response).to redirect_to(root_path)
+        end
+
+        context "when user logs out twice from different browsers" do
+          
+          before { delete logout_path }
+
+          it "should keep the user logged out" do
+            follow_redirect!
+            expect(response.body).to include("Log In")
+            expect(response.body).to include("Sign Up")
+          end          
+        end
+      end
+    end
+
+    context "when remember is checked" do
+      it "should log the user in and create remember token" do
+        log_in_as(@user, remember_me: '1')
+        expect(cookies['remember_token']).to eq(assigns(:user).remember_token)
+      end
+    end
+
+    context "when remember is not checked" do
+      it "should only log the user in" do
+        log_in_as(@user, remember_me: '0')
+        expect(cookies['remember_token']).to be(nil)
+      end
+    end
   end
 end
