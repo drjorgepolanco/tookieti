4112d6542a51223946bce53abe93041deb26cbbb
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-04-03 14:50:08 -0400

    Add first associations in User model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,9 @@
 class Recipe < ActiveRecord::Base
   belongs_to :user
+  has_many   :likes
+  validates  :title,       presence: true, length: { maximum: 100 }
+  validates  :description, presence: true, length: { minimum: 10  }
+  validates  :steps,       presence: true, length: { minimum: 10  }
+  validates  :user_id,     presence: true
+  default_scope -> { order(updated_at: :desc) }
 end
