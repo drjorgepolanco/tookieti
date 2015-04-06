8ec5dd820a6277390c37d126a4dde1dafb573b0b
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-04-06 17:25:59 -0400

    Add constraint to avoid users updating other user's profile. Add flash message for that. Add method set_user() for show, edit, update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [        :edit, :update          ]
+  before_action :set_user,       only: [:show,  :edit, :update          ]
   before_action :admin_user,     only: [                        :destroy]
   
   def show
-    @user = User.find(params[:id])
     @recipes = @user.recipes.paginate(page: params[:page], per_page: 15)
     redirect_to(root_url) and return unless @user.activated
   end
@@ -29,11 +29,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Tu perfil fue actualizado satisfactoriamente."
       redirect_to(@user)
@@ -59,6 +57,10 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
+    def set_user
+      @user = User.find(params[:id])
+    end
+
     def logged_in_user
       unless logged_in?
         store_location
@@ -70,6 +72,7 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+      flash[:alert] = "Solo puedes modificar tu propio perfil."
     end
 
     def admin_user
