1058d024c786677b7531a1a6ac65364cb17f1af6
JORGE POLANCO <DR.JORGEPOLANCO@GMAIL.COM>
2015-04-07 18:39:05 -0400

    Create models Cuisine and Ingredient

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150407162606) do
+ActiveRecord::Schema.define(version: 20150407223816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cuisines", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ingredients", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "likes", force: true do |t|
     t.boolean  "like"
